41ebf5ecd8d7ebc77db6cc7c14041c91dce896de
Vinod.marnur <Vinod.marnur@HMECD000721.happiestminds.com>
2018-10-04 17:03:17 +0530

    angular order page changes

--- spring-boot-rest-hibernate/src/main/webapp/WEB-INF/static/angular/my-app/src/app/shared/customer.ts
+++
@@ -1,7 +0,0 @@
-export class Customer {
-   
-    constructor(public customerId:number,public customerName:string,public email:string,public mobileNo:string,public dob:string)
-    {
-        
-    }
-}
--- spring-boot-rest-hibernate/src/main/webapp/WEB-INF/static/angular/my-app/src/app/shared/employee.ts
+++
@@ -1,9 +0,0 @@
-export class Employee {
-  
-    constructor(public id:number,public name:string,public email:string,public address:string,public countryId:number,public stateId:number,public cityId:number)
-    {
-        
-    }
-
-
-}
